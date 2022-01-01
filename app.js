76daa89f7b9832d707daa9b4d9fbc65ef735c7d5
MianAnswer <miananswer@hotmail.com>
2021-12-31 19:04:12 -0500

    Create express app and use ejs as view engine

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log('Hello World');
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
+
+app.get('/', (req, res, next) => {
+    res.render('home');
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Listening to port: ${port}`)
+});
