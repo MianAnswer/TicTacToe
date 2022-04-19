4ab4f919fe9a5968b9c5f447f6be0fc4f4acb961
MianAnswer <miananswer@hotmail.com>
2022-04-19 19:20:37 -0400

    Update Game logic

--- public/scripts/tictactoe_game.js
+++ public/scripts/tictactoe_game.js
@@ -1,28 +1,67 @@
-var prepareBoard = function () {
-    var board = document.getElementById('tictactoe-board');
+const symbols = ['X', 'O'];
+const board = [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
+];
+let index = 0;
+
+const prepareBoard = function () {
+    const board = document.getElementById('tictactoe-board');
 
     for (let row = 0; row < 3; ++row) {
         const rowDiv = document.createElement('div');
         rowDiv.classList.add('row');
         for (let col = 0; col < 3; ++col) {
-            const colDiv = document.createElement('div');
+            const cellDiv = document.createElement('div');
+            cellDiv.setAttribute('row', row);
+            cellDiv.setAttribute('col', col);
 
-            colDiv.classList.add('col');
-            colDiv.style.width = `${board.clientWidth / 3}px`;
-            colDiv.style.height = `${board.clientHeight / 3}px`;
-            colDiv.addEventListener('click', function (e) {
-                colDiv.appendChild(document.createTextNode('X'));
-                colDiv.style.pointerEvents = 'none';
-            });
+            initCell(board, cellDiv);
 
-            rowDiv.appendChild(colDiv);
+            rowDiv.appendChild(cellDiv);
         }
 
         board.appendChild(rowDiv);
     }
 }
 
-var readyStateCheckInterval = setInterval(function () {
+const initCell = function (board, cellDiv) {
+    cellDiv.classList.add('cell');
+    cellDiv.style.width = `${board.clientWidth / 3}px`;
+    cellDiv.style.height = `${board.clientHeight / 3}px`;
+    cellDiv.addEventListener('click', function (e) {
+        addSymbolToCell(cellDiv);
+    });
+}
+
+const addSymbolToCell = function (cellDiv) {
+    const symbol = getSymbol();
+
+    addSymbolToBoard(cellDiv, symbol);
+
+    cellDiv.appendChild(document.createTextNode(symbol));
+    cellDiv.style.pointerEvents = 'none';
+}
+
+const getCellPosition = function (cellDiv) {
+    return { row: Number(cellDiv.getAttribute('row')), col: Number(cellDiv.getAttribute('col')) };
+}
+
+const addSymbolToBoard = function (cellDiv, symbol) {
+    const { row, col } = getCellPosition(cellDiv);
+    board[row][col] = symbol;
+}
+
+const getSymbol = function () {
+    if (index >= symbols.length) {
+        index = 0;
+    }
+
+    return symbols[index++];
+}
+
+const readyStateCheckInterval = setInterval(function () {
     if (document.readyState === "complete") {
         clearInterval(readyStateCheckInterval);
         prepareBoard();
